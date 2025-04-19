5857e4574999b98141f9e8ff4b7e473e157c327f
Satishkumar-Jadhav21 <satishkumar.jadhav21@gmail.com>
2025-04-20 02:48:56 +0530

    Fixed: Removed submodule and added frontend as normal folder

---
+++ frontend/src/landing-page/OpenAccount.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function OpenAccount() {
+  return (
+    <div className="container p-5 mb-5">
+      <div className="row text-center">
+        <h1 className="mt-5">Open a Zerodha account</h1>
+        <p>
+          Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and
+          F&O trades.
+        </p>
+        <button
+          className="p-2 btn btn-primary fs-5 mb-5"
+          style={{ width: "20%", margin: "0 auto" }}
+        >
+          <Link to="http://localhost:3001/register" className="text-white text-decoration-none">
+          Sign up Now
+          </Link>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default OpenAccount;
